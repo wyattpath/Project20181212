6f31cb3e6febc06b96fc2555cc053cd40ce755ac
viethoang92 <vi_hoang@hotmail.de>
2018-12-23 01:55:57 +0100

    Update AttackTrigger.cs
    target only loses health if it is not in hurting stage

--- Assets/Scripts/AttackTrigger.cs
+++ Assets/Scripts/AttackTrigger.cs
@@ -2,22 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackTrigger : MonoBehaviour 
+public class AttackTrigger : MonoBehaviour
 {
-	public int attackDamage = 20;
+    public int attackDamage = 20;
 
-	public LayerMask whatIsPlayer;
-	public float size = 0f;
+    public LayerMask whatIsPlayer;
+    public float size = 0f;
 
-	
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if(!other.isTrigger && other.CompareTag("Player"))
-		{
-			PlayerHealth targetHealth = other.GetComponent<PlayerHealth>();
-			targetHealth.TakeDamage(attackDamage);
-			Debug.Log(targetHealth.currentHealth);
-		}
-	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.isTrigger && other.CompareTag("Player"))
+        {
+            PlayerHealth targetHealth = other.GetComponent<PlayerHealth>();
+            if (!targetHealth.hurting)
+            {
+                targetHealth.TakeDamage(attackDamage);
+                Debug.Log(targetHealth.currentHealth);
+            }
+        }
+    }
 }
