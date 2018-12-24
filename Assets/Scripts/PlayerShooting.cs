bf0caa78fee30b7060b202cac0f3d5b7f39a2064
viethoang92 <vi_hoang@hotmail.de>
2018-12-24 05:30:41 +0100

    Added Shooting Animation

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     public int playerNumber = 1;
+    public float shootCooldown = 0.5f;
+    public float shootTriggerTimer = 0.1f;
     public Transform firePoint;
     public GameObject fireballPrefab;
 
+    private float shootTimer;
     private string playerShootName;
     private Animator animator;
+    private bool shooting = false;
 
     private void Awake()
     {
@@ -23,10 +27,29 @@ public class PlayerShooting : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetButtonDown(playerShootName))
+        if (Input.GetButtonDown(playerShootName) && !shooting)
         {
-            Shoot();    
+            shooting = true;
+            shootTimer = shootCooldown;
+            animator.SetTrigger("shoot");
         }
+
+        if (shooting)
+        {
+            if (shootTimer > 0)
+            {
+                shootTimer -= Time.deltaTime;
+
+                if (shootTimer < shootTriggerTimer)
+                {
+                    Shoot();
+                    shooting = false;
+                }
+            }
+        }
+
+        animator.SetBool("shooting", shooting);
+
     }
 
     void Shoot()
