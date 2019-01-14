9da7c45897a973276dfd1811834f41be0eb0e9fc
viethoang92 <vi_hoang@hotmail.de>
2019-01-14 14:39:48 +0100

    added PlayerMana and adjusted Animation Speed

---
+++ Assets/Scripts/PlayerMana.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMana : MonoBehaviour
+{
+    public float startingMana = 100f;
+    public float currentMana = 0f;
+    public float manaRegenerationPoints = 20f;
+    public float manaRegenerationCooldown = 1f;
+
+    private float manaTimer;
+
+    void OnEnable()
+    {
+        currentMana = startingMana;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentMana < startingMana)
+        {
+            if (manaTimer > 0f)
+            {
+                manaTimer -= Time.deltaTime;
+            }
+            else
+            {
+                currentMana += manaRegenerationPoints;
+            }
+        }
+        else
+        {
+            currentMana = startingMana;
+        }
+    }
+
+    public void decreaseMana(int amount)
+    {
+        manaTimer = manaRegenerationCooldown;
+        currentMana -= amount;
+    }
+
+    public bool canShoot(int amount)
+    {
+        return (currentMana - amount >= 0);
+    }
+}
--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     public int playerNumber = 1;
-    public float shootCooldown = 1.0f;
-    public float shootTriggerTimer = 0.6f;
+    public float shootCooldown = 1f;
+    private float shootTriggerTimer = 0.6f;
     public Transform firePoint;
     public GameObject fireballPrefab;
+    public int manaCost = 20;
 
     private float shootTimer = -1.0f;
     private string playerShootName;
     private Animator animator;
     private bool shooting = false;
+    private PlayerMana playerMana;
+
+    // Mana
 
     private void Awake()
     {
@@ -23,35 +27,39 @@ public class PlayerShooting : MonoBehaviour
     private void Start()
     {
         playerShootName = "Fire" + playerNumber;
+        playerMana = GetComponent<PlayerMana>();
+
+        // animation speed
+        animator.speed /= shootCooldown;
+        shootTriggerTimer *= shootCooldown;
     }
 
     void Update()
     {
         if (Input.GetButtonDown(playerShootName) && !shooting && shootTimer <= 0)
         {
-            shooting = true;
-            shootTimer = shootCooldown;
-            animator.SetTrigger("shoot");
+            if (playerMana.canShoot(manaCost))
+            {
+                playerMana.decreaseMana(manaCost);
+                shooting = true;
+                shootTimer = shootCooldown;
+                animator.SetTrigger("shoot");
+            }
         }
-
         if (shootTimer > 0)
         {
             shootTimer -= Time.deltaTime;
-
-            if (shooting)
+        }
+        if (shooting)
+        {
+            if (shootTimer < shootTriggerTimer)
             {
-                if (shootTimer < shootTriggerTimer)
-                {
-                    Shoot();
-                    shooting = false;
-                }
+                Shoot();
+                shooting = false;
             }
         }
-
         animator.SetBool("shooting", shooting);
-
     }
-
     void Shoot()
     {
         Instantiate(fireballPrefab, firePoint.position, firePoint.rotation);
